ace04f2e9ce76ecbe9a530ada9f2f5dc5d0ae97a
arthurdeandradehygino-crypto <arthurdeandradehygino@gmail.com>
2026-01-14 18:27:31 -0300

    Javascript more currencies

--- scripts.js
+++ scripts.js
@@ -2,8 +2,10 @@
 
 const botao = document.querySelector(".botão")
 
-const dolar = 5.2
-const euro = 6.2
+const dolar = 5.39
+const euro = 6.28
+const libra = 7.24
+const bitcoin = 525753.54
 
 const moedaAconverter = document.querySelector(".moeda-a-converter")
 const moedaConvertida = document.querySelector(".moeda-convertida")
@@ -40,11 +42,49 @@ function converter() {
 
     }
 
+    if (currencySelect == 'libra') {
+
+
+        moedaConvertida.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP"
+        }).format(valorAconverter / libra)
+
+
+
+    }
+
+    if (currencySelect == 'bitcoin') {
+
+
+        moedaConvertida.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: "decimal",
+            currency: "BTC",
+            minimumFractionDigits: 8,
+            maximumFractionDigits: 8
+        }).format(valorAconverter / bitcoin)
+
+
+
+    }
+
+
+
+
+
     moedaAconverter.innerHTML = new Intl.NumberFormat("pt-br", {
         style: "currency",
-        currency: "BRL"
+        currency: "BRL", 
+        
     }).format(valorAconverter)
 }
+
+
+
+
+
+
+
 
 
 
@@ -71,11 +111,32 @@ function textoModificar() {
     }
 
 
+    if (currencySelect.value == 'libra') {
+
+        mudartexto.innerHTML = "libra"
+        imgCurrency.src = './assets/libra 1.png'
+        converter()
+    }
+
+if (currencySelect.value == 'bitcoin') {
+
+        mudartexto.innerHTML = "Bitcoin"
+        imgCurrency.src = './assets/bitcoin 1.png'
+        converter()
+    }
+
+
+
 }
 
 currencySelect.addEventListener("change", textoModificar)
 botao.addEventListener('click', converter)
 
+document.addEventListener("keypress", function (e) {
+
+    if (e.key === "Enter") { converter() }
+
+})
 
 
 
